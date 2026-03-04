230f31b3050e4ac93c5533618fa44cb06170c5f2
Akash Patel <akash7071@gmail.com>
2026-03-04 17:12:03 -0600

    changed webpage to quote display and hid counter behind http://localhost:8000/counters

--- web_logger.py
+++ web_logger.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, redirect, request
 from services.counters_service import mark_done, get_status
+from auth.keep_client import get_random_quote
 from datetime import date
 
 app = Flask(__name__)
 
-# Homepage showing all tasks
+# Homepage showing quote
 @app.route("/")
 def index():
+    quote = get_random_quote()
+    return render_template("quote.html", quote=quote)
+
+# Counters page
+@app.route("/counters")
+def counters():
     statuses = get_status()
-    return render_template("index.html", statuses=statuses, date=date)
+    return render_template("counters.html", statuses=statuses, date=date)
 
 # Mark a task done (default today)
 @app.route("/mark/<task>")
 def mark(task):
     mark_done(task, source="web")
-    return redirect("/")
+    return redirect("/counters")
 
 # Mark a task with a custom date
 @app.route("/mark_custom", methods=["POST"])
@@ -23,7 +30,7 @@ def mark_custom():
     date_str = request.form["date"]
 
     mark_done(task, event_date=date_str, source="web")
-    return redirect("/")
+    return redirect("/counters")
 
 
 if __name__ == "__main__":
